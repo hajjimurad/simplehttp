13e4442d98694e86918be2beecd25e26959a3fbd
murad <muradm@bk.ru>
2017-02-26 23:13:35 +0100

    updated package

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ var http = require('http');
 var finalhandler = require('finalhandler');
 var serveStatic = require('serve-static');
 
-var serve = serveStatic("./data/");
+var serve = serveStatic("/var/www/tiles/data");
 
 var server = http.createServer(function(req, res){
-    var done = finalhandler(req, res)
-    serve(req, res, done)
+    var done = finalhandler(req, res);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    serve(req, res, done);
 });
 
 server.listen(80);
